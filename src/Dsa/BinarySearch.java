e6ffcfefb18dff345a5db2c2c831479d83ca3627
cliff-codes <108398436+cliff-codes@users.noreply.github.com>
2024-02-13 05:57:39 -0800

    Binary Search implemented

--- src/Dsa/BinarySearch.java
+++ src/Dsa/BinarySearch.java
@@ -1,4 +1,35 @@
 package Dsa;
 
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,5,6,7,8,9};
+
+        int result = binary_search(numbers, 9);
+        System.out.println("result is at index"+ result);
+    }
+
+    public static int binary_search(int[] list, int target){
+          int first = 0;
+          int last = list.length-1;
+
+          while (first <= last){
+            int mid = (first + last) / 2;
+
+              System.out.println(mid);
+
+              if(list[mid] == target){
+                  return mid;
+              }
+
+              //if target is greater, ignore left half
+              if(list[mid] < target){
+                  first = mid + 1;
+              }
+              //else if target is smaller, ignore right half
+              else {
+                  last = mid -1;
+              }
+          }
+          return -1;
+    }
 }
